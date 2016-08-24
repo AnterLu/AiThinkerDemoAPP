cf7c9a56b788db1b15f874d8a4685eafe1e8c419
AnterLu <3252451320@qq.com>
2016-08-24 18:27:55 +0800

    Initial Commit

---
+++ AiThinkerDemoAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AiThinkerDemoAPP
+//
+//  Created by Anter_Mac on 16/8/24.
+//  Copyright © 2016年 Anter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AiThinkerDemoAPP/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  AiThinkerDemoAPP
+//
+//  Created by Anter_Mac on 16/8/24.
+//  Copyright © 2016年 Anter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
